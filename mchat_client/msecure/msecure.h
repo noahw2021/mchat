633cffeea7a6867c68cdc4597ceeccadf8983920
Noah Wooten <noah.wooten@icloud.com>
2023-12-31 23:44:21 -0500

    update msecure header

--- mchat_client/msecure/msecure.h
+++ mchat_client/msecure/msecure.h
@@ -10,8 +10,29 @@
 
 #include "../types.h"
 
-typedef struct _MSEC_USER {
-    WORD64 a;
-}MSEC_USER, *PMSEC_USER;
+typedef struct _MSC_KEY {
+    WORD64 KeyBase;
+    WORD64 KeyRelocs[32];
+    WORD64 KeyHash;
+    
+    WORD64 KeyData[128];
+}MSC_KEY, *PMSC_KEY;
+
+typedef struct _MSC_USER {
+    WORD64 UserHash[2];
+    STR Alias[64];
+}MSC_USER, *PMSC_USER;
+
+typedef struct _MSC_USER_FILE {
+    STR Username[64];
+    WORD64 PasswordToken[2];
+}MSC_USER_FILE, *PMSC_USER_FILE;
+
+// Crypt Functions
+void MsccMessageEncrypt(MSC_KEY Key, void* Buffer, WORD32 Length);
+void MsccMessageDecrypt(MSC_KEY Key, void* Buffer, WORD32 Length);
+
+// User Functions
+void MscuActivateUser(PMSC_USER User, MSC_USER_FILE UserFile);
 
 #endif /* msecure_h */
