3e1e0c0e9a3a30e90081c4d3ec6f0612a1fce55c
Noah Wooten <noah.wooten@icloud.com>
2024-01-10 15:30:02 -0500

    rename to recvevents

---
+++ mchat_client/chat/chat_recvevents.c
@@ -0,0 +1,76 @@
+//
+//  chat_events.c
+//  mchat_client
+//
+//  Created by Noah Wooten on 1/9/24.
+//
+
+#include "chat.h"
+#include "../netw/netw.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+int ChatGetEventList(void) {
+    char* Request = malloc(256);
+    sprintf(Request, "api0/Events_GetList?Secret="
+        "%016llX%016llX%016llX%016llX", ChatCtx->MySecret[0],
+        ChatCtx->MySecret[1], ChatCtx->MySecret[2], ChatCtx->MySecret[3]);
+    
+    char* Response = NetwGETApi(Request);
+    free(Request);
+    
+    PCHAT_LIST List = malloc(sizeof(CHAT_LIST));
+    NetwBase64Decode(List, sizeof(CHAT_LIST), Response);
+    free(Response);
+    NetwDecrypt256(List, sizeof(CHAT_LIST), ChatCtx->ClientKey);
+    
+    if (ChatCtx->PreviousList)
+        free(ChatCtx->PreviousList);
+    
+    ChatCtx->PreviousList = List;
+    
+    return List->EventCount;
+}
+
+int ChatGetEventType(int i) {
+    if (!ChatCtx->PreviousList)
+        return -1;
+    
+    return ChatCtx->PreviousList->EventTypes[i];
+}
+
+void* ChatiGetEvent(int i) {
+    char* Request = malloc(256);
+    sprintf(Request, "api0/Events_GetObject?Secret="
+        "%016llX%016llX%016llX%016llX&EventId=%i", ChatCtx->MySecret[0],
+        ChatCtx->MySecret[1], ChatCtx->MySecret[2], ChatCtx->MySecret[3],
+        i);
+    
+    char* Response = NetwGETApi(Request);
+    free(Request);
+    
+    void* Buffer = malloc(CHATSZ_MAX);
+    NetwBase64Decode(Buffer, CHATSZ_MAX, Response);
+    free(Response);
+    NetwDecrypt256(Buffer, CHATSZ_MAX, ChatCtx->ClientKey);
+    
+    return Buffer;
+}
+
+PCHAT_YES ChatGetEventAsYes(int i) {
+    return ChatiGetEvent(i);
+}
+
+PCHAT_MESSAGE ChatGetEventAsMessage(int i) {
+    return ChatiGetEvent(i);
+}
+
+
+PCHAT_RECVUSERNAME ChatGetEventAsRecvUsername(int i) {
+    return ChatiGetEvent(i);
+}
+
+PCHAT_LOGINRECV ChatGetEventAsRecvLogin(int i) {
+    return ChatiGetEvent(i);
+}
---
+++ mchat_client/chat/chat_sendevents.c
@@ -0,0 +1,8 @@
+//
+//  chat_sendevents.c
+//  mchat_client
+//
+//  Created by Noah Wooten on 1/10/24.
+//
+
+#include <stdio.h>
