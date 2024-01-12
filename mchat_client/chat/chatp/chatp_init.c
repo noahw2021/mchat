c9159fe53f1af09a6c78f952529bb43a5bb0ac5d
Noah Wooten <noah.wooten@icloud.com>
2024-01-12 10:00:55 -0500

    stage changes for previous commits

---
+++ mchat_client/chat/chatp/chatp_init.c
@@ -0,0 +1,8 @@
+//
+//  chatp_init.c
+//  mchat_client
+//
+//  Created by Noah Wooten on 1/12/24.
+//
+
+#include <stdio.h>
---
+++ mchat_client/chat/chatp/chatp_send.c
@@ -0,0 +1,8 @@
+//
+//  chatp_send.c
+//  mchat_client
+//
+//  Created by Noah Wooten on 1/12/24.
+//
+
+#include <stdio.h>
