1f7f521a8ae01e74772316061551c9ab7fcb5a25
Noah Wooten <noah.wooten@icloud.com>
2024-01-24 09:37:32 -0500

    remove platform files

--- mchat_client/netw/platform/platform_init.c
+++
@@ -1,8 +0,0 @@
-//
-//  platform_init.c
-//  mchat_client
-//
-//  Created by Noah Wooten on 1/24/24.
-//
-
-#include <stdio.h>
--- mchat_client/netw/platform/platform_report.c
+++
@@ -1,8 +0,0 @@
-//
-//  platform_report.c
-//  mchat_client
-//
-//  Created by Noah Wooten on 1/24/24.
-//
-
-#include <stdio.h>
